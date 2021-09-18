8b5441af3b8b697ff5f6a5d6ee1b53a389890bf3
julietvt <freshitpt@gmail.com>
2021-09-18 11:45:20 +0300

    style card

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,21 +3,31 @@ const users = [
     id: 1,
     name: 'name1',
     surname: 'surname1',
-    imgSrc: '#',
+    imgSrc:
+      'https://i.pinimg.com/originals/8f/5a/79/8f5a7910b9d97bb15fd315bcb7111d2a.jpg',
   },
   {
     id: 2,
     name: 'name2',
     surname: 'surname2',
-    imgSrc: '#',
+    imgSrc:
+      'https://www.computerra.ru/wp-content/uploads/2019/12/posjk5RPMqk.jpg',
   },
   {
     id: 3,
     name: 'name3',
     surname: 'surname3',
-    imgSrc: '#',
+    imgSrc:
+      'https://oir.mobi/uploads/posts/2021-04/thumbs/1619786354_50-oir_mobi-p-dovolnii-kotik-zhivotnie-krasivo-foto-52.jpg',
   },
 ];
+
+//получение списка с разметки в body
+const userListElem = document.getElementById('userList');
+// для каждого обьекта в массиве создается новая li в общем списке ul
+users.forEach((user) => {
+  userListElem.append(createUserListItemElem(user));
+});
 
 function createUserFullName({ name, surname }) {
   const userFullNameElem = document.createElement('h2');
@@ -25,12 +35,12 @@ function createUserFullName({ name, surname }) {
   return userFullNameElem;
 }
 
-function createUserImage({ imageSrc }) {
+function createUserImage({ imgSrc }) {
   const userImgContElem = document.createElement('div');
   userImgContElem.classList.add('userImgContainer');
   const userImageElem = document.createElement('img');
   userImageElem.classList.add('userImage');
-  userImageElem.setAttribute('src', user.imageSrc);
+  userImageElem.setAttribute('src', imgSrc);
   userImageElem.setAttribute('alt', 'userphoto');
   userImgContElem.append(userImageElem);
   return userImgContElem;
