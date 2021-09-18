d17c81b4b5d7630c068e9e8830b0f3eb6ae0e559
julietvt <freshitpt@gmail.com>
2021-09-18 14:02:02 +0300

    board structure create

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,11 @@
 'use script';
 
-let sliderEl = document.getElementById('slider');
-let thumbEL = document.getElementById('thumb');
+//let board = document.getElementById('container')
+let board = document.querySelector('#container');
+const NUMBER = 208;
 
-thumbEL.onmousedown = function (e) {
-  e.preventDefault();
-  let shift = e.clientX - thumbEL.getBoundingClientRect().left;
-  document.addEventListener('mousemove', onMouseMove);
-  document.addEventListener('mouseup', onMouseUp);
-
-  function onMouseMove(e) {
-    let x = e.clientX - shift - sliderEl.getBoundingClientRect().left;
-    if (x < 0) x = 0;
-    // style width у элмента slider sliderEl.offsetWidth
-    let rightX = sliderEl.offsetWidth - thumbEL.offsetWidth;
-    if (x > rightX) x = rightX;
-    thumbEL.style.left = x + 'px';
-  }
-
-  function onMouseUp(e) {
-    document.removeEventListener('mouseup', onMouseUp);
-    document.removeEventListener('mousemove', onMouseMove);
-  }
-};
+for (let i = 0; i < NUMBER; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+  board.append(square);
+}
