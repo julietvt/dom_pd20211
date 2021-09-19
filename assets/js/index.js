818af83450060d6a224e4707415f039c0e97d9ca
julietvt <freshitpt@gmail.com>
2021-09-19 10:45:15 +0300

    color board v1

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,16 @@
-'use script';
+const board = document.getElementById('board');
+const COUNT_SQUARES = 416;
 
-//let board = document.getElementById('container')
-let board = document.querySelector('#container');
-const NUMBER = 208;
-
-for (let i = 0; i < NUMBER; i++) {
+for (let i = 0; i < COUNT_SQUARES; i++) {
   const square = document.createElement('div');
   square.classList.add('square');
+  square.addEventListener('mouseover', () => {
+    square.style.backgroundColor = 'green';
+    square.style.boxShadow = '0 0 10px black';
+  });
+  square.addEventListener('mouseleave', () => {
+    square.style.backgroundColor = 'gray';
+    square.style.boxShadow = '0 0 2px black';
+  });
   board.append(square);
 }
